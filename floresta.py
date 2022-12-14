17c4257fe949228cd182daa7f57d82737ba6a002
Bella <72763583+BelaRocha@users.noreply.github.com>
2022-12-14 10:48:49 -0300

    Update floresta.py

--- floresta.py
+++ floresta.py
@@ -2,31 +2,48 @@
 import data
 
 data.animar('Pudim adentra a floresta, uma densa vegetação ao seu redor.')
+input()
+data.clear()
 if data.temPena == False:
-    print('Ele sente uma vontade repentina de subir uma árvore, mas se controla. É quando ele nota, parado num galho, um pombo distraído!\nDar o bote?')
-##1-sim/2-nao
+    data.animar('Ele sente uma vontade repentina de subir uma árvore, mas se controla. É quando ele nota, parado num galho, um pombo distraído!\nDar o bote?')
+    data.animar('')
+    data.animar('\n\n1-Atacar           2-Passar despercebido')
+    escolha = data.valInt('\n             ')
     data.animar('Você pula no pombo e arranca uma pena.')
+    input()
+    data.clear()
     data.falas('txtes/pombo', 'Pombo', 'Ei, o que pensa que tá fazendo? Prometo que vou te processar, seu gato imundo!')
     data.animar('O Pombo parece bravo. Ele sai voando para longe.')
+    input()
+    data.clear()
     data.falas('txtes/pena', 'Pudim conseguiu a Pena Suja!', '')
     data.temPena == True
     data.animar('Você não faz nada. Parece uma oportunidade desperdiçada.')
+    input()
+    data.clear()
 if data.temFlor == False:
     data.animar('Pudim continua o seu caminho pela floresta, passando entre as arvores enquanto procura a flor até que encontra um Soldado Rato no caminho!\
         \nEle parece distraido cortando lenha, o que fazer?')
-    #(1-ataque\2-passar despercebido)
+    data.animar('\n\n1-Atacar           2-Passar despercebido')
+    escolha = data.valInt('\n             ')
     data.animar("Você pula no inimigo, o atacando de surpresa!") 
-    #luta
+    data.menu_luta
     data.animar('...O rato cai derrotado, Pudim segue em frente novamente!') #sucesso
+    input()
+    data.clear()
     data.animar('Você tenta se camuflar nas árvores e passar despercebido...')
     data.animar('...Mas Pudim é branco! O rato percebe sua presença e te ataca!') 
-    #luta
+    data.menu_luta
     data.animar('...E mesmo Pudim sendo branco, o rato é dautônico! Pudim passa de fininho e avança!') #sucesso
+    input()
+    data.clear()
 
     data.animar('Pudim anda por o que parecem horas para suas perninhas curtas de gatinho, até que, num caminho ingrime e escuro, ele vê,\
         \niluminada por um unico feiche de luz que corta pela copa das árvores, a Flor Ancestral! Pudim pula em sua direção, porém!')
         #luta com dinoplanta
     data.animar('A Dinoplanta mítica de lenda cai derrotada de volta ao chão! Você segura a Flor Ancestral em suas patas')
+    input()
+    data.clear()
     data.falas('txtes/flor', '(Pudim pegou a Flor Ancestral!)', '')
     data.temFlor = True
     if data.ingredientesFalta == 0:
